3111c6f6a07a64ec77e344265ae9ace76cdbc88f
Crafter <mylinkmail@foxmail.com>
2024-03-07 16:00:53 +0800

    jpg2webp

---
+++ Go/jpg2webp.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	dir := "C:\\Users\\admin\\Pictures\\新建文件夹" // 要处理的目录
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		ext := strings.ToLower(filepath.Ext(path))
+		if ext == ".jpg" || ext == ".png" || ext == ".jpeg" {
+			output := filepath.Base(path)
+			output = output[0:len(output)-len(filepath.Ext(output))] + ".webp"
+
+			outputPath := filepath.Join(filepath.Dir(path), output)
+
+			cmd := exec.Command("ffmpeg", "-i", path, outputPath)
+			err := cmd.Run()
+			if err != nil {
+				log.Printf("Error converting file: %s", err)
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Printf("Error walking the directory: %s", err)
+	}
+}
